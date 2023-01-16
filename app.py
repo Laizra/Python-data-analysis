5d2e585e7fcf5353e85cf1b7afc9ca0f4bdfbac7
Laizra <lluzzra@gmail.com>
2023-01-16 15:55:21 -0600

    string and number basics + math

---
+++ app.py
@@ -0,0 +1,13 @@
+character_name = "John"
+character_age = "25"
+print(character_name + " was cool")
+print("He was also " + character_age)
+character_name = "Tom"
+print(character_name + " liked video games")
+print("He was a pro by " + character_age)
+
+phrase = "Tall Giraffe Academy"
+print(phrase.replace("Giraffe", "Goose"))
+print("\"Giraffe\nAcademy\"")
+
+print(pow(4, 6))
