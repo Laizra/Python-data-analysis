4338d742acca9eafa3f4e11e9dd23acdfbb6c95f
Laizra <lluzzra@gmail.com>
2023-01-16 20:57:31 -0600

    comparisons with if statements

--- list.py
+++ list.py
@@ -37,3 +37,16 @@ elif not (is_blue) and is_pink:
     print("is not blue but is pink")
 else:
     print("not blue nor pink")
+# ----------------------------------comparisons-------------------
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(300, 57, 983))
