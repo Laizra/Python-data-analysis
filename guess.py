29ccccd593995d06b9ec08b1617e666300e36b48
Laizra <lluzzra@gmail.com>
2023-01-17 15:44:06 -0600

    exponent function

--- guess.py
+++ guess.py
@@ -15,3 +15,14 @@ if out_of_guesses:
     print("Out of gueses, YOU LOSE")
 else:
     print("You win!")
+# --------------------------exponent function-----------------
+
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+
+print(raise_to_power(2, 3))
