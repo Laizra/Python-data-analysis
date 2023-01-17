9d77fa7d97d524df0e62b94e671ce7d1636d8154
Laizra <lluzzra@gmail.com>
2023-01-16 19:40:56 -0600

    tuple and funtion def

--- list.py
+++ list.py
@@ -5,3 +5,15 @@ friends.insert(1, "Liv")
 print(friends[0:])
 
 print(friends[1])
+# ----------------------Tuples-------------------------
+coordinates = (4, 5)
+print(coordinates[0])
+# ----------------------Functions-----------------------
+
+
+def say_hi(name, age):
+    print("Hello " + name + " You're " + age)
+
+
+say_hi("Derek", "19")
+say_hi("Steve", "20")
