c509f3e465f963c0c4dae8d54639744b3a59c6a5
Laizra <lluzzra@gmail.com>
2023-01-16 19:15:41 -0600

    fixed a mistake

--- list.py
+++ list.py
@@ -1,8 +1,7 @@
 lucky_numbers = [4, 8, 15, 16, 23, 42]
 friends = ["Kie", "Levi", "Lisa", "alex", "jay", "stefan"]
 friends.insert(1, "Liv")
-<<<<<<< HEAD
+
 print(friends[0:])
-=======
+
 print(friends[1])
->>>>>>> 3660cdcd03336d76d576eaf8ff40339da1e48a36
