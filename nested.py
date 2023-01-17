b638bf2a98be158ee8e7b77e71f94c9bdc2bb22d
Laizra <lluzzra@gmail.com>
2023-01-17 16:30:33 -0600

    2D list, translator

---
+++ nested.py
@@ -0,0 +1,27 @@
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
+# ----------------------------translator-------------
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter phrase: ")))
