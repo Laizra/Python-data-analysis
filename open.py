60b4c7590b0b3ef11ffb327e7ee205971eea882d
Laizra <lluzzra@gmail.com>
2023-01-17 21:44:35 -0600

    classes and objects. student definition

--- open.py
+++ open.py
@@ -2,3 +2,12 @@ employee_file = open("employees.txt", "r")
 for employee in employee_file.readlines():
     print(employee)
 employee_file.close()
+# ------------classes-----------
+
+
+class Student:
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
---
+++ student.py
@@ -0,0 +1,5 @@
+from open import Student
+
+Student1 = Student("Jim", "Business", 3.1, False)
+
+print(Student1.name)
